e926dec4792824807a7dcb1765a35493ddd5eaa0
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-06 02:40:00 +0900

    move example.rb

--- example/example.rb
+++ example/example.rb
@@ -1,14 +1,6 @@
-require "lib/blf"
+$LOAD_PATH.push('./lib')
+require "./lib/blf"
 
-world = World.new(500, 500)
-
-# world.add_initial_block
-(1..30).each do |i|
-  world.add_block width: random(10, 30), height: random(10, 30)
-end
-
-world.draw_up_blocks
-
-def random(min, max)
-  Random.new(Random.new_seed) * (max - min) + min
-end
+world = World.new width: 500, height: 500
+world.add_block start_x: 0, start_y: 0, width: 50, height: 50
+world.draw
--- test.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.push('./lib')
-require "./lib/blf"
-
-world = World.new width: 500, height: 500
-world.add_block start_x: 0, start_y: 0, width: 50, height: 50
-world.draw
