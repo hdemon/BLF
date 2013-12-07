d16a4f0c1c1d41bd4fc15f2f768744ab7e2e6baf
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-08 08:35:47 +0900

    enable to allocate blocks to the world

--- example/example.rb
+++ example/example.rb
@@ -2,6 +2,16 @@ $LOAD_PATH.push('./lib')
 require "./lib/blf"
 
 world = World.new width: 500, height: 500
-world.add_block_with_location start_x: 0, start_y: 0, width: 50, height: 50
-# world.add_block width: 25, height: 50
+
+# 座標を指定してブロックを追加する。
+world.add_block_with_location start_x: 60, start_y: 0, width: 50, height: 50
+
+(1..50).each do |n|
+  w = Random.new(Random.new_seed).rand * 100 + 10
+  h = Random.new(Random.new_seed).rand * 100 + 10
+  # 座標を指定せずにブロックを追加し、詰め込みの対象とする。
+  world.add_block width: w, height: h
+end
+
+world.allocate_all
 world.draw
